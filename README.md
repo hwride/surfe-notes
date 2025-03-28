91645253fb6de8a018b41ae726fdd4aea4766abe
Hugo Wride <hugowride@gmail.com>
2025-03-28 17:17:22 +0000

    Add prettier

--- README.md
+++ README.md
@@ -1,11 +1,14 @@
 # Surfe Notes
 
 ## Development
+
 1. Install dependencies: `npm install`
 2. Run the dev server: `npm run dev`
 
 ## Technologies
+
 - React
 - Vite
 - TypeScript
 - Tailwind
+- Prettier
--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,11 @@
-import './App.css'
+import "./App.css";
 
 function App() {
   return (
     <>
       <h1 className="text-3xl font-bold underline">Surfe Notes</h1>
     </>
-  )
+  );
 }
 
-export default App
+export default App;
