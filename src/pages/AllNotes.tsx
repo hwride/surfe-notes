f4f12d241769b36794c3b859821462b7da479dfe
Hugo Wride <hugowride@gmail.com>
2025-03-29 09:45:39 +0000

    Style all notes and login a little

--- src/pages/AllNotes.tsx
+++ src/pages/AllNotes.tsx
@@ -21,22 +21,26 @@ export function AllNotes({ sessionId }: { sessionId: string }) {
   }, []);
 
   return (
-    <main className="max-w-100 m-auto">
-      <h1 className="text-3xl font-bold m-auto text-center mb-2">
-        Surfe Notes
-      </h1>
-      {state.state === "loading" ? "Loading..." : null}
-      {state.state === "loaded" ? (
-        <ol>
-          {state.notes.map((note) => (
-            <li key={note.id}>
-              <a href={`?sessionId=${sessionId}&noteId=${note.id}`}>
-                Note {note.id}
-              </a>
-            </li>
-          ))}
-        </ol>
-      ) : null}
-    </main>
+    <div className="bg-[#e6eef6]">
+      <main className="max-w-180 h-dvh p-8 m-auto flex flex-col">
+        <h1 className="text-3xl font-bold m-auto text-center mb-2">
+          Your notes
+        </h1>
+        <div className="flex-1">
+          {state.state === "loading" ? "Loading..." : null}
+          {state.state === "loaded" ? (
+            <ol>
+              {state.notes.map((note) => (
+                <li key={note.id}>
+                  <a href={`?sessionId=${sessionId}&noteId=${note.id}`}>
+                    Note {note.id}
+                  </a>
+                </li>
+              ))}
+            </ol>
+          ) : null}
+        </div>
+      </main>
+    </div>
   );
 }
