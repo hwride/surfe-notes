f3ce6030685fede2b442212010da1966f65c38b4
Hugo Wride <hugowride@gmail.com>
2025-03-29 08:00:52 +0000

    Only save a note after the user stops typing

---
+++ src/config/config.ts
@@ -0,0 +1,2 @@
+/** How long to wait after the user stops typing to save a note. */
+export const noteSaveTimeoutMs = 250;
--- src/pages/Note.tsx
+++ src/pages/Note.tsx
@@ -1,6 +1,7 @@
 import { NoteType } from "../types/NoteType.ts";
-import { useEffect, useState } from "react";
-import { getNote } from "../services/note-service.ts";
+import { useEffect, useRef, useState } from "react";
+import { getNote, saveNote } from "../services/note-service.ts";
+import { noteSaveTimeoutMs } from "../config/config.ts";
 
 export function Note({
   sessionId,
@@ -14,6 +15,7 @@ export function Note({
   >({
     state: "loading",
   });
+  const saveTimeout = useRef<number | null>(null);
 
   // Fetch initial note state.
   useEffect(() => {
@@ -36,21 +38,26 @@ export function Note({
         <textarea
           className="w-full border"
           value={state.note.body}
-          onChange={(e) => {
+          onChange={async (e) => {
             const newBody = e.target.value;
-            setState((state) => {
-              if (state.state === "loaded") {
-                return {
-                  ...state,
-                  note: {
-                    ...state.note,
-                    body: newBody,
-                  },
-                };
-              } else {
-                return state;
+            const newNote = {
+              id: noteId,
+              body: newBody,
+            };
+            setState({ state: "loaded", note: newNote });
+
+            // Save the note to the sever X ms after the user stops typing.
+            // We reset the timer each time the user types something else.
+            if (saveTimeout.current) {
+              clearTimeout(saveTimeout.current);
+            }
+            saveTimeout.current = window.setTimeout(async () => {
+              try {
+                await saveNote(sessionId, noteId, newNote);
+              } catch (e) {
+                console.error("Failed to save note", e);
               }
-            });
+            }, noteSaveTimeoutMs);
           }}
         />
       ) : null}
